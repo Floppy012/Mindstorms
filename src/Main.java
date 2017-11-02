fafcc5f1f99e63e000f7355e756496c85f0aec53
Florian Schmidt <fschmidtbusiness@gmail.com>
2017-11-02 19:48:28 +0100

    Better mainclass

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,29 @@ public class Main {
 	public static void main(String[] args) {
 		RobotController controller = new RobotController();
 
+		testMatrix(controller);
+	}
+
+	private static void testMarquee(RobotController controller) {
 		char[] directions = new char[] { 'l', 'r', 't', 'b' };
 
 		for (char c : directions) {
 			controller.marquee("Test", c);
 		}
+	}
 
+	private static void testBatteryStatus(RobotController controller) {
+		controller.showBatteryState();
+
+		try {
+			Thread.sleep(2000L);
+		} catch (InterruptedException ex) {
+			//Nothing
+		}
+	}
+
+	private static void testMatrix(RobotController controller) {
+		controller.matrix();
 	}
 
 }
